21a099f6aa945a3c56382a6ec02894a79da80d3e
Yavor Mihaylov <yavor.st.m@gmail.com>
2020-06-23 00:58:50 +0300

    WIP

--- app/Commands/FetchNewOffers.php
+++ app/Commands/FetchNewOffers.php
@@ -14,14 +14,14 @@ class FetchNewOffers extends Command
      *
      * @var string
      */
-    protected $signature = 'fetcher:fetch-new-offers';
+    protected $signature = 'fetch-new-offers';
 
     /**
      * The description of the command.
      *
      * @var string
      */
-    protected $description = 'Fetch new offers from the websites';
+    protected $description = 'Fetch new offers from the websites and notify!';
 
     /**
      * Execute the console command.
@@ -32,11 +32,15 @@ class FetchNewOffers extends Command
     {
         $dom = new Dom();
         /** @var \Illuminate\Http\Client\Response $reponse */
-        $response = Http::get('https://www.olx.bg/nedvizhimi-imoti/prodazhbi/apartamenti/oblast-sofiya-grad/?search%5Bfilter_float_price%3Ato%5D=80000&search%5Bfilter_enum_atype%5D%5B0%5D=2&search%5Bfilter_enum_atype%5D%5B1%5D=3&search%5Bfilter_float_space%3Afrom%5D=80&search%5Bfilter_float_cyear%3Ato%5D=2020&search%5Bfilter_enum_ctype%5D%5B0%5D=tuhla&search%5Bfilter_enum_floor%5D%5B0%5D=2&search%5Bfilter_enum_floor%5D%5B1%5D=3&search%5Bfilter_enum_floor%5D%5B2%5D=4&search%5Bfilter_enum_floor%5D%5B3%5D=5&search%5Bfilter_enum_floor%5D%5B4%5D=6');
+        $response = Http::get('https://www.olx.bg/nedvizhimi-imoti/prodazhbi/apartamenti/oblast-sofiya-grad/?search%5Bfilter_float_price%3Ato%5D=80000&search%5Bfilter_enum_atype%5D%5B0%5D=2&search%5Bfilter_enum_atype%5D%5B1%5D=3&search%5Bfilter_float_space%3Afrom%5D=80&search%5Bfilter_float_cyear%3Ato%5D=2020&search%5Bfilter_enum_ctype%5D%5B0%5D=tuhla&search%5Bfilter_enum_floor%5D%5B0%5D=2&search%5Bfilter_enum_floor%5D%5B1%5D=3&search%5Bfilter_enum_floor%5D%5B2%5D=4&search%5Bfilter_enum_floor%5D%5B3%5D=5&search%5Bfilter_enum_floor%5D%5B4%5D=6&search%5Bfilter_enum_nlf%5D%5B0%5D=1');
+
         $html = $dom->load($response->body());
-        $tds = $html->find('#offers_table tr');
-//        $tds->each()
-        die;
+
+        $allOffers = $html->find('div.offer-wrapper');
+        foreach ($allOffers as $offerHtml) {
+            $linkToOffer = $offerHtml->find('.title-cell')[0]->find('a')[0]->getAttribute('href');
+            $titleOfOffer = trim(strip_tags($offerHtml->find('.title-cell')[0]->find('a')[0]->innerHtml));;
+        }
     }
 
     /**
